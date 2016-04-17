e8b75a260c8ce43967b3ccae5e42e363d4a832cb
Chairman Wang <simuwang@usc.edu>
2016-04-17 16:47:43 -0700

    quality of life improvements

--- MovieKnight/app/src/main/java/com/example/nathan/movieknight/activities/RegisterActivity.java
+++ MovieKnight/app/src/main/java/com/example/nathan/movieknight/activities/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.nathan.movieknight.R;
 
@@ -27,7 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
         guestbutton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-
+                        EditText displayname = (EditText)findViewById(R.id.displayName);
                     }
                 }
         );
