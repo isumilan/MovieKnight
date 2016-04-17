94a2f4839d4c786cbadf0e17e93bba6031d5e715
Chaitanya Penumetsa <penumets@usc.edu>
2016-04-17 11:37:56 -0700

    Added MovieInfo, MovieResults and updated Gradle

---
+++ MovieKnight/app/src/main/java/com/example/nathan/movieknight/MovieInfo.java
@@ -0,0 +1,78 @@
+package com.example.nathan.movieknight;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by chaitanyap7 on 4/15/16.
+ */
+public class MovieInfo {
+
+    @SerializedName("original_title")
+    private String movieTitle;
+
+    @SerializedName("overview")
+    private String movieOverview;
+
+    @SerializedName("tagline")
+    private String movieTagline;
+
+    @SerializedName("poster_path")
+    private String moviePosterPath;
+
+    @SerializedName("runtime")
+    private String movieRuntime;
+
+    @SerializedName("vote_average")
+    private String movieVoteAverage;
+
+    @SerializedName("vote_count")
+    private String movieVoteCount;
+
+    @SerializedName("genres")
+    private List<Genre> movieGenres;
+
+
+    public String getTitle() {
+        return movieTitle;
+    }
+
+    public String getOverview() {
+        return movieOverview;
+    }
+
+    public String getTagLine() {
+        return movieTagline;
+    }
+
+
+    public String getPosterPath() {
+        return moviePosterPath;
+    }
+
+    public String getRuntime() {
+        return movieRuntime;
+    }
+
+    public String getVoteAverage() {
+        return movieVoteAverage;
+    }
+
+    public String getVoteCount() {
+        return movieVoteCount;
+    }
+
+    public List<Genre> getGenres() {
+        return movieGenres;
+    }
+
+    public static class Genre{
+        @SerializedName("name")
+        private String movieName;
+
+        public String getName() {
+            return movieName;
+        }
+    }
+}
