3c45417fa01f6308cb41ac6f2cc9c4f89d522969
Isumi Lan <Isumi Lan>
2016-04-18 09:00:40 +0800

    no message

--- MovieKnight_Server/src/server/ServerClientDialogue.java
+++ MovieKnight_Server/src/server/ServerClientDialogue.java
@@ -1,8 +1,12 @@
-package server;
 
 public class ServerClientDialogue {
 	private int requestType;
 	private Object DialogueContent;
+	
+	public ServerClientDialogue(int type, Object content){
+		requestType = type;
+		DialogueContent = content;
+	}
 	
 	public int getRequestType(){
 		return requestType;
