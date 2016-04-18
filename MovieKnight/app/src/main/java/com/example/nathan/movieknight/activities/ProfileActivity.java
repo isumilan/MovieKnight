c60c6ef4827d9740e7dabdd62d48632d60f74c19
Chairman Wang <simuwang@usc.edu>
2016-04-17 17:34:36 -0700

    added button functionality to profile, register

--- MovieKnight/app/src/main/java/com/example/nathan/movieknight/activities/ProfileActivity.java
+++ MovieKnight/app/src/main/java/com/example/nathan/movieknight/activities/ProfileActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.nathan.movieknight.NavigationDrawer;
 import com.example.nathan.movieknight.R;
@@ -19,8 +21,6 @@ public class ProfileActivity extends NavigationDrawer {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -29,6 +29,51 @@ public class ProfileActivity extends NavigationDrawer {
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //this button should apppear for other users but not self
+        //ie getIntent.getExtras.getBoolean(friend) == true
+        Button addfriendbutton = (Button)findViewById(R.id.addFriendButton);
+        addfriendbutton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //sends a friend request
+                        //makes a popup notifying success
+                    }
+                }
+        );
+
+        //this button should apppear for self but not other users
+        //ie getIntent.getExtras.getBoolean(friend) == false
+        Button editbutton = (Button)findViewById(R.id.editButton);
+        editbutton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //open up some kind of editing interface
+                        //stuff to edit: (in order of priority)
+                        //Profile Description
+                        //Favorite Movies
+                        //Profile Image
+                    }
+                }
+        );
+
+        Button friendlistbutton = (Button)findViewById(R.id.friendListButton);
+        friendlistbutton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //opens up the friend list activity
+                    }
+                }
+        );
+
+        Button movielistbutton = (Button)findViewById(R.id.movieListButton);
+        movielistbutton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //open up the movie list activity
+                    }
+                }
+        );
     }
 
 
